60f9f970885c6074c68f1b3baa6ef9af7db7e3f5
DorinGradinaru <dorin.gradinaru@icloud.com>
2024-03-15 10:50:42 +0200

    first commit

---
+++ arrow.js
@@ -0,0 +1,16 @@
+//NUMARA PANA LA 10
+// const countTo = (n) => {
+//     for (let i = 0; i <= n; i++) {
+//       console.log(i);
+//     }
+//   };
+//   countTo(10);
+  
+
+  // AFISEAZA LITERELE
+const displayLetters = (string) => {
+    for (let i = 0; i < string.length; i++) {
+      console.log(string[i]);
+    }
+  };
+  displayLetters('buna ziua');
---
+++ index.js
@@ -0,0 +1,31 @@
+//NUMARA PANA LA 10 CU WHILE
+
+// let i = 0
+// while (i <= 10) {
+//     console.log(i)
+//     i++
+// }
+
+
+
+//NUMARA PANA LA 10 CU DO WHILE
+
+// let i = 0
+// do {
+//     console.log(i)
+//     i++
+// } while (i <= 10)
+
+
+
+
+
+//AFISEAZA FIECARE LITERA
+
+// let string = 'buna ziua';
+// let i = 0;
+
+// do {
+//   console.log(string[i]);
+//   i++;
+// } while (i < string.length);
